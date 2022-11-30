0c9fe2f963223e587ae6ca43c053f1ca2c23fff7
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-11-30 04:54:57 -0800

    task8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for alpha in str:
+        if ord(alpha) >= ord('a') and ord(alpha) <= ord('z'):
+            alpha = chr(ord(alpha) - 32)
+    print("{:s}".format(alpha), end="")
+print("")
