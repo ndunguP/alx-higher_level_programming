bdac35357382d61a6a312ef6c528bdc8cf2af89a
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-11-30 07:09:15 -0800

    task5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,5 +2,5 @@
 for number in range(0, 100):
     if number == 99:
         print("{}".format(number))
-else:
-    print("{:02d},".format(number), end=",")
+    else:
+        print("{:02d},".format(number), end=",")
