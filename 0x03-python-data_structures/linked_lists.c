f3f3940a53d506a4050940eeaabc11dd034ab0db
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-12-05 00:58:04 -0800

    task13

--- 0x03-python-data_structures/linked_lists.c
+++ 0x03-python-data_structures/linked_lists.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+#include <stddef.h>
 
 /**
 * print_listint - prints all elements of a listint_t list
@@ -9,35 +10,18 @@
 */
 
 size_t print_listint(const listint_t *h)
-
 {
-
-	    const listint_t *current;
-
-	        unsigned int n; /* number of nodes */
-
-
-
-		    current = h;
-
-		        n = 0;
-
-			    while (current != NULL)
-
-				        {
-
-						        printf("%i\n", current->n);
-
-							        current = current->next;
-
-								        n++;
-
-									    }
-
-
-
-			        return (n);
-
+const listint_t *current;
+unsigned int n; /* number of nodes */
+current = h;
+n = 0;
+while (current != NULL)
+{
+printf("%i\n", current->n);
+current = current->next;
+n++;
+}
+return (n);
 }
 
 /**
@@ -50,54 +34,24 @@ size_t print_listint(const listint_t *h)
 listint_t *add_nodeint_end(listint_t **head, const int n)
 
 {
-
-	    listint_t *new;
-
-	        listint_t *current;
-
-
-
-		    current = *head;
-
-
-
-		        new = malloc(sizeof(listint_t));
-
-			    if (new == NULL)
-
-				            return (NULL);
-
-
-
-			        new->n = n;
-
-				    new->next = NULL;
-
-
-
-				        if (*head == NULL)
-
-						        *head = new;
-
-					    else
-
-						        {
-
-								        while (current->next != NULL)
-
-										            current = current->next;
-
-									        current->next = new;
-
-										    }
-
-
-
-					        return (new);
-
+listint_t *new;
+listint_t *current;
+current = *head;
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
+new->next = NULL;
+if (*head == NULL)
+*head = new;
+else
+{
+while (current->next != NULL)
+current = current->next;
+current->next = new;
 }
-
-
+return (new);
+}
 
 /**
 * free_listint - frees a listint_t list
@@ -106,23 +60,12 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 */
 
 void free_listint(listint_t *head)
-
 {
-
-	    listint_t *current;
-
-
-
-	        while (head != NULL)
-
-			    {
-
-				            current = head;
-
-					            head = head->next;
-
-						            free(current);
-
-							        }
-
+listint_t *current;
+while (head != NULL)
+{
+current = head;
+head = head->next;
+free(current);
+}
 }
