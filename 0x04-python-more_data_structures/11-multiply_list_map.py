6dbb02dd97b8cc82b63ce937f36be956f163389e
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-12-07 08:04:42 -0800

    lists

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-prst_map(my_list=[], number=0):
-    return list(map(lambda x: x * number, my_list))int("Buzz ", end="")
+def mutiply_list_map(my_list=[], number=0):
+    return list(map(lambda x: x * number, my_list))
