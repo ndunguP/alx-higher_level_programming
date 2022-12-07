7d62407139082f17f95520038d003b9107a4c82f
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-12-07 05:55:33 -0800

    squarematrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    '''Ccomputes the square value of all integers of a matrix'''
-        return [[i**2 for i in row] for row in matrix]
+    return [[i**2 for i in row] for row in matrix]
