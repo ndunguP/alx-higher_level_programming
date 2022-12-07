03d78b806f27e0e4587411bf1e73a19741da4687
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-12-07 05:17:49 -0800

    lists

---
+++ 0x04-python-more_data_structures/lists.h
@@ -0,0 +1,28 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stddef.h>
+/**
+ *struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ * for project
+ */
+typedef struct listint_s
+{
+int n;
+struct listint_s *next;
+} listint_t;
+def square_matrix_simple(matrix=[]):
+	def search_replace(my_list, search, replace):
+		def uniq_add(my_list=[]):
+			def common_elements(set_1, set_2):
+				def only_diff_elements(set_1, set_2):
+					def number_keys(a_dictionary):
+						def print_sorted_dictionary(a_dictionary):
+							def update_dictionary(a_dictionary, key, value):
+								def simple_delete(a_dictionary, key=""):
+						
+#endif /* LISTS_H */
+
+
