ca042f778afc4a392ea3129cdbf1be279b778ea6
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2023-01-02 04:58:49 -0800

    test_driven

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Divide matrix elements module"""
+
+
+def matrix_divided(matrix, div):
+    """matrix_divided - divied a matrix
+    
+        Args:
+            matrix - list of lists
+            div - number matrix elements are to be divided by
+        Returns:
+            new matrix with divided elements
+    """
+    error = "matrix must be a matrix (list of lists) of integers/floats"
+    if type(matrix) is not list or matrix is None:
+        raise TypeError(error)
+    new_list = []
+    if type(div) is not int and type(div) is not float or div is None:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    for i in matrix:
+        if len(matrix[0]) != len(i):
+            raise TypeError("Each row of the matrix must have the same size")
+        if type(i) is not list:
+            raise TypeError(error)
+        for j in i:
+            if (type(j) is not int and type(j) is not float):
+                raise TypeError(error)
+    new_list = [[round(x / div, 2) for x in row]for row in matrix]
+    return new_list
---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module to print string"""
+
+
+def say_my_name(first_name, last_name=""):
+    """say_my_name - print string
+    
+    Args:
+        first_name - first name to print
+        last_name - last name to print
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
