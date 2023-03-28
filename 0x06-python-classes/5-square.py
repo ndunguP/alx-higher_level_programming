7d7267dc605e899f12f065a5dd912de03edb221a
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2023-03-28 07:22:38 -0700

    fixd

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,18 +1,39 @@
 #!/usr/bin/python3
-class Square:
-    """A Square class."""
+'''
+Square class definition
+'''
 
+
+class Square():
+    '''
+    Square class with private attribute size
+    '''
     def __init__(self, size=0):
-    """Initialize the size of square."""
-    self.size = size
+        '''
+        Args:
+            size: size of the square
+        '''
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
     @property
     def size(self):
-        """Getter for size of square."""
+        '''
+        Returns the size
+        '''
         return self.__size
 
     @size.setter
     def size(self, value):
-        "Setter for size of square."""
+        '''
+        Sets the size of the square
+        Args:
+            value: value to be evaluated
+        '''
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -21,14 +42,14 @@ class Square:
             self.__size = value
 
     def area(self):
-        """Calculate and return area of square."""
+        '''
+        Returns area of the square
+        '''
         return self.__size ** 2
 
     def my_print(self):
-        """"Print the square to stdout."""
-        if self.__size <= 0:
-            print("\n", end="")
-        else:
-            row = "#" * self.size
-            for i in range(self.size):
-                print("{}".format(row))
+        '''
+        Prints out the square
+        '''
+        row = ''.join('#' for c in range(self.size))
+        print('\n'.join(row for r in range(self.size)))
