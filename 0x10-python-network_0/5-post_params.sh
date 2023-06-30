36ff2a4b49f23ca4787f6aa735c5b64748001a0c
ndunguP <peter.ndunguoz50@gmail.com>
2023-06-30 13:29:45 -0700

    fixed

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a POST request and displays the body of the response
-curl -sd "email=test@gmail.com.com&subject=I will always be here for PLD" $1
+curl -sd "email=test@gmail.com&subject=I will always be here for PLD" $1
