04417079680850e6f93a5d227c398b4b95cdd0b6
ndunguP <peter.ndunguoz50@gmail.com>
2023-06-30 17:17:37 -0700

    init

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""fetch URL module using command line argument"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = dict(response.info())
+
+    print(html.get("X-Request-Id"))
