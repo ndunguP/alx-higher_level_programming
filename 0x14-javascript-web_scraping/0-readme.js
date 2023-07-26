ef15fcd4bdc503a664cefb56c494a945f2ef66bb
ndunguP <peter.ndunguoz50@gmail.com>
2023-07-25 17:12:40 -0700

    init commit

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/* Script that reads and prints the content of a file */
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (err, contents) {
+  if (err) { console.log(err); } else { console.log(contents); }
+});
